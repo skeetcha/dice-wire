46523c04eda4d965e9737cb371df8cc672a7b56d
Cass Unterholzner <cityboundforest@gmail.com>
2024-05-23 11:09:04 -0500

    Adding some stuff for managing a monster's stats

--- combat/battlers/jobs/startingStats.gd
+++ combat/battlers/jobs/startingStats.gd
@@ -1,6 +1,16 @@
 extends Resource
-
 class_name StartingStats
 
-@export var job_name : String = "job"
-@export var max_health : int
+enum SIZES {TINY, SMALL, MEDIUM, LARGE, HUGE, GARGANTUAN}
+enum TYPES {ABBERATION, BEAST, CELESTIAL, CONSTRUCT, DRAGON, ELEMENTAL, FEY, FIEND, GIANT, HUMANOID, MONSTROSITY, OOZE, PLANT, UNDEAD}
+
+@export var job_name : String = "Job"
+@export var size : SIZES
+@export var type : TYPES
+@export var subtype : String
+# Only used for overrides on monsters
+@export var ac : int
+@export var speeds : Dictionary = {"walking": 0, "climbing": 0, "flying": 0, "swimming": 0}
+@export var abilityScores : Dictionary = {"str": 0, "dex": 0, "con": 0, "int": 0, "wis": 0, "cha": 0}
+@export var currentHP : float
+@export var hitDice : Dictionary = {"amount": 0, "size": 0, "bonus": 0}
