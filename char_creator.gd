e6fb99653671ec35361dd8560a55d73729d42631
Cass Unterholzner <cityboundforest@gmail.com>
2024-05-29 09:45:10 -0500

    Race, Class, and Background buttons added; need to add explainers for each option; going to add color changes to stock model for each change

--- char_creator.gd
+++ char_creator.gd
@@ -2,7 +2,11 @@ extends Control
 
 var race: int = 0
 var class_val: int = 0
+var background: int = 0
 var panel: int = 0
+
+func _ready():
+	pass
 
 func race_button(val: int, toggle: bool):
 	if race == 0 and toggle:
@@ -23,6 +27,16 @@ func class_button(val: int, toggle: bool):
 		$Grid/MiddlePanel.visible = false
 	else:
 		class_val = val
+
+func background_button(val: int, toggle: bool):
+	if background == 0 and toggle:
+		background = val
+		$Grid/MiddlePanel.visible = true
+	elif background == val and not toggle:
+		background = 0
+		$Grid/MiddlePanel.visible = false
+	else:
+		background = val
 
 func _on_dwarf_button_toggled(toggled_on):
 	race_button(1, toggled_on)
@@ -61,8 +75,14 @@ func _on_next_button_pressed():
 	elif panel == 1:
 		assert(class_val != 0)
 		$Grid/ClassPanel.visible = false
+		$Grid/BackgroundPanel.visible = true
 		$Grid/MiddlePanel.visible = false
 		panel = 2
+	elif panel == 2:
+		assert(background != 0)
+		$Grid/BackgroundPanel.visible = false
+		$Grid/MiddlePanel.visible = false
+		panel = 3
 
 func _on_barbarian_button_toggled(toggled_on):
 	class_button(1, toggled_on)
@@ -99,3 +119,66 @@ func _on_warlock_button_toggled(toggled_on):
 
 func _on_wizard_button_toggled(toggled_on):
 	class_button(12, toggled_on)
+
+func _on_acolyte_button_toggled(toggled_on):
+	background_button(1, toggled_on)
+
+func _on_artisan_button_toggled(toggled_on):
+	background_button(2, toggled_on)
+
+func _on_charlatan_button_toggled(toggled_on):
+	background_button(3, toggled_on)
+
+func _on_criminal_button_toggled(toggled_on):
+	background_button(4, toggled_on)
+
+func _on_cultist_button_toggled(toggled_on):
+	background_button(5, toggled_on)
+
+func _on_entertainer_button_toggled(toggled_on):
+	background_button(6, toggled_on)
+
+func _on_exile_button_toggled(toggled_on):
+	background_button(7, toggled_on)
+
+func _on_farmer_button_toggled(toggled_on):
+	background_button(8, toggled_on)
+
+func _on_folk_hero_button_toggled(toggled_on):
+	background_button(9, toggled_on)
+
+func _on_gambler_button_toggled(toggled_on):
+	background_button(10, toggled_on)
+
+func _on_guard_button_toggled(toggled_on):
+	background_button(11, toggled_on)
+
+func _on_guild_member_button_toggled(toggled_on):
+	background_button(12, toggled_on)
+
+func _on_hermit_button_toggled(toggled_on):
+	background_button(13, toggled_on)
+
+func _on_marauder_button_toggled(toggled_on):
+	background_button(14, toggled_on)
+
+func _on_noble_button_toggled(toggled_on):
+	background_button(15, toggled_on)
+
+func _on_outlander_button_toggled(toggled_on):
+	background_button(16, toggled_on)
+
+func _on_sage_button_toggled(toggled_on):
+	background_button(17, toggled_on)
+
+func _on_sailor_button_toggled(toggled_on):
+	background_button(18, toggled_on)
+
+func _on_soldier_button_toggled(toggled_on):
+	background_button(19, toggled_on)
+
+func _on_trader_button_toggled(toggled_on):
+	background_button(20, toggled_on)
+
+func _on_urchin_button_toggled(toggled_on):
+	background_button(21, toggled_on)
